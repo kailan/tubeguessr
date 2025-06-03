ca4b8abfd947b68e9c857991ccca6686a6f98df1
Kailan Blanks <kailan@enviark.com>
2025-06-03 15:33:37 +0100

    Fix referer policy

--- compute-js/src/index.js
+++ compute-js/src/index.js
@@ -52,7 +52,7 @@ async function handleRequest(event) {
       response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
       response.headers.set("X-Content-Type-Options", "nosniff");
       response.headers.set("X-Frame-Options", "SAMEORIGIN");
-      response.headers.set("Referrer-Policy", "no-referrer");
+      response.headers.set("Referrer-Policy", "same-origin");
       response.headers.set(
         "Content-Security-Policy",
         "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self'; connect-src 'self';"
